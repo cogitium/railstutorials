ab7ef084370550b5742d34a8c5d79066bdd97668
Philippe Giraud <info@cogitium.com>
2016-02-25 12:39:03 +0100

    Initialize repository

---
+++ Manipultations.rb
@@ -0,0 +1,45 @@
+# Manipulations du livre Railstutorials
+# A partir du chapitre 3
+
+# 3.1
+
+# Après s'être positionné dans la zone de travail
+# Il fautcréer une nouvelle application
+$ rails new railstutorials
+$ cd railstutorials
+
+# Changer le contenu du Gemfile par ceci :
+source 'https://rubygems.org'
+
+gem 'rails',        '4.2.2'
+gem 'sass-rails',   '5.0.2'
+gem 'uglifier',     '2.5.3'
+gem 'coffee-rails', '4.1.0'
+gem 'jquery-rails', '4.0.3'
+gem 'turbolinks',   '2.3.0'
+gem 'jbuilder',     '2.2.3'
+gem 'sdoc',         '0.4.0', group: :doc
+
+group :development, :test do
+  gem 'sqlite3',     '1.3.9'
+  gem 'byebug',      '3.4.0'
+  gem 'web-console', '2.0.0.beta3'
+  gem 'spring',      '1.1.3'
+end
+
+group :test do
+  gem 'minitest-reporters', '1.0.5'
+  gem 'mini_backtrace',     '0.1.3'
+  gem 'guard-minitest',     '2.3.1'
+end
+
+group :production do
+  gem 'pg',             '0.17.1'
+  gem 'rails_12factor', '0.0.2'
+end
+
+# Bundle install sans la production
+$ bundle install --without production
+$ bundle update
+
+
