563bce9cb4855455a5dfa973bbd77086113edea5
Philippe Giraud <info@cogitium.com>
2017-01-20 16:01:53 +0100

    Modif nom de domaine dans mail sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   #host = '<your heroku app>.herokuapp.com'
-  host = 'https://ancient-bastion-62755.herokuapp.com/'
+  #host = 'https://ancient-bastion-62755.herokuapp.com/'
+  host = 'https://railstutorials-phg.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
